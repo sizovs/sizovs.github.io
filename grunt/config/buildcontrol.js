7c0463506e4f75426f21196cad788342956cbbaa
Eduards Sizovs <eduards@sizovs.net>
2016-12-15 15:54:06 +0200

    connect commits disabled

--- grunt/config/buildcontrol.js
+++ grunt/config/buildcontrol.js
@@ -3,6 +3,7 @@ module.exports = {
     dir: 'dist',
     commit: true,
     push: true,
+    connectCommits: false,
     message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
   },
   pages: {
